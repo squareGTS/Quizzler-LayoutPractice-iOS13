53079877d4880c5040928fc745683e1b6fd492a3
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-07-31 10:08:39 +0300

    Quizzler
    IOS App Design Patterns and Code Structuring

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle! //True or Faulse
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+    
+    Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        let answerChoices = quizBrain.getAnswerText()
+        
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+        Button1.backgroundColor = UIColor.clear
+        Button2.backgroundColor = UIColor.clear
+        Button3.backgroundColor = UIColor.clear
+        
+        Button1.setTitle(answerChoices[0], for: .normal)
+        Button2.setTitle(answerChoices[1], for: .normal)
+        Button3.setTitle(answerChoices[2], for: .normal)
+    }
+}
+
+
